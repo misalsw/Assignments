1a3f375211128fb8ac3f1a991481e9fefcb73213
swap123 <misalswapnil77@gmail.com>
2022-02-19 08:14:36 +0530

    if & Else

---
+++ selection/4.sh
@@ -0,0 +1,9 @@
+#Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+headOrtail=$((RANDOM%2))
+if [ $headOrtail  -eq 0 ]
+then
+   echo "head"
+else
+   echo "tail"
+fi
