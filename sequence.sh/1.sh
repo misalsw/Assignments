d07a611af26d11cdc4b908caf23e3894c96f472e
swap123 <misalswapnil77@gmail.com>
2022-02-19 07:59:59 +0530

    Sequence practice problem

---
+++ sequence.sh/1.sh
@@ -0,0 +1,4 @@
+#Use Random Function (( RANDOM )) to get Single Digit
+
+randomCheck=$((RANDOM%2))
+echo "$randomCheck"
---
+++ sequence.sh/2.sh
@@ -0,0 +1,4 @@
+#Use Random to get Dice Number between 1 to 6
+
+randomCheck=$((RANDOM%7))
+echo "$randomCheck"
